2904ea671948ea4a6114d16969585a2191bc5252
Noah Englesma <engelsmanoah@gmail.com>
2025-03-27 13:22:50 -0400

    Second Hoop - Stable

--- Tello_Competition.py
+++ Tello_Competition.py
@@ -61,12 +61,11 @@ try:
 
 # Commit Message: First hoop - Stable
 
-        sendmsg("go 190 0 35 100")
-
+        sendmsg("go 185 7 28 100")
 
 # Commit Message: Second Hoop - Stable
 
-        sendmsg("go 210 0 50 100")
+        sendmsg("go 195 10 32 100")
 
 
 # Commit Message: Third Hoop - Stable
